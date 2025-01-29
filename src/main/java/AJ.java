a884b1ac2e699bc1d372d49dcfb7a0cdf642ecb3
juneja999 <akkij2004@gmail.com>
2025-01-29 16:00:29 +0800

    Change name of Bot to AJ

---
+++ src/main/java/AJ.java
@@ -0,0 +1,11 @@
+public class AJ {
+    public static void main(String[] args) {
+        String brLine = "----------------------------------------------------";
+        System.out.println(brLine);
+        System.out.println("Hi there! I'm AJ, always happy to help :) ");
+        System.out.println("What's on your mind?");
+        System.out.println(brLine);
+        System.out.println("PS:In case I don't see you later, good afternoon, good evening, and good night");
+        System.out.println(brLine);
+    }
+}
