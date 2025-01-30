760f1ed6bd1780bc77ec527a05a293e0c745e4a6
juneja999 <akkij2004@gmail.com>
2025-01-31 00:54:59 +0800

    Add echo functionality of bot
    The bot echoes user command and exits when commanded with "bye"

--- src/main/java/AJ.java
+++ src/main/java/AJ.java
@@ -1,11 +1,38 @@
+import java.util.Scanner;
+
+
 public class AJ {
+
+    private final static String DOTTED_LINE="***********************(^‿^)***************************";
+    private final static int AJ_TEXT_INDENTATION = 7; //will use with " ".repeat(#)
+
+    //used to segregate user and bot text
+    public static void getDottedLine(){
+        System.out.println(DOTTED_LINE+"\n");
+    }
+
+
     public static void main(String[] args) {
-        String brLine = "----------------------------------------------------";
-        System.out.println(brLine);
+        getDottedLine();
         System.out.println("Hi there! I'm AJ, always happy to help :) ");
         System.out.println("What's on your mind?");
-        System.out.println(brLine);
+        getDottedLine();
         System.out.println("PS:In case I don't see you later, good afternoon, good evening, and good night");
-        System.out.println(brLine);
+        getDottedLine();
+
+        String userCommand;
+        while(true){
+            Scanner input = new Scanner(System.in);
+            userCommand = input.nextLine();
+            getDottedLine();
+            if(!userCommand.equals("bye")){
+                System.out.println(" ".repeat(AJ_TEXT_INDENTATION)+userCommand);
+            }else{
+                System.out.println(" ".repeat(AJ_TEXT_INDENTATION) + "Bye,take care! May your device's battery never run out.");
+                getDottedLine();
+                break;
+            }
+            getDottedLine();
+        }
     }
 }
